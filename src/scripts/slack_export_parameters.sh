9fefb2ad2c4509480ceee80748eae90151e6b2d8
Travis Fielding <travis@sourcepoint.com>
2022-06-02 11:20:44 -0400

    use slack condition instead of script

--- src/scripts/slack_export_parameters.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo 'export SLACK_MESSAGE_HEADER="${PARAM_MESSAGE_HEADER}"' >> $BASH_ENV
-echo 'export SLACK_GIT_URL="${PARAM_GIT_URL}"' >> $BASH_ENV
-if [ "${PARAM_LINK_TO_WORKFLOW}" = "true" ]
-then
-  echo 'export CIRCLE_CI_ACTION_LINK="${PARAM_CIRCLE_CI_HOST}/workflow-run/${PARAM_WORKFLOW_ID}"' >> $BASH_ENV
-else
-  echo 'export CIRCLE_CI_ACTION_LINK="${PARAM_BUILD_URL}"' >> $BASH_ENV
-fi
