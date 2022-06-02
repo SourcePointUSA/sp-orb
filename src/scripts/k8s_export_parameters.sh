b5bb4ee041ac3209404610e3d766f4ac48ff7a78
Travis Fielding <travis@sourcepoint.com>
2022-06-02 12:46:08 -0400

    switch to conditions instead of using script

--- src/scripts/k8s_export_parameters.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo Deploying to k8s "${PARAM_CLUSTER_NAME}" cluster
-if [ "${PARAM_CLUSTER_NAME}" = "pl-stage" ]
-then
-  echo 'export KUBE_AWS_REGION="us-east-1"' >> $BASH_ENV
-else
-  echo 'export KUBE_AWS_REGION="eu-central-1"' >> $BASH_ENV
-fi
-echo 'export ARTIFACT_VERSION="${PARAM_GIT_SHA1}"' >> $BASH_ENV
