ffc4e11ebc1e16132c5574c5fc2044d71211d80e
Travis Fielding <travis@sourcepoint.com>
2022-06-03 13:57:45 -0400

    add nix_docker_build_and_push_image support

---
+++ src/scripts/nix_build_and_push_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+FLAKE_DOCKER_IMAGE_NAME=$(eval echo "$PARAM_FLAKE_DOCKER_IMAGE_NAME")
+
+nix build
+set -e
+sudo chmod -R 777 /nix/store/$(ls /nix/store/ | grep crates-io | grep -v '.drv$')
+nix build
+nix build .#${FLAKE_DOCKER_IMAGE_NAME}
+docker load < result 
+docker tag ${FLAKE_DOCKER_IMAGE_NAME}:latest ${AWS_ECR_ACCOUNT_URL}/${FLAKE_DOCKER_IMAGE_NAME}:latest
+docker push ${AWS_ECR_ACCOUNT_URL}/${FLAKE_DOCKER_IMAGE_NAME}:latest
+docker tag ${FLAKE_DOCKER_IMAGE_NAME}:latest ${AWS_ECR_ACCOUNT_URL}/${FLAKE_DOCKER_IMAGE_NAME}:0.0.${CIRCLE_BUILD_NUM}
+docker push ${AWS_ECR_ACCOUNT_URL}/${FLAKE_DOCKER_IMAGE_NAME}:0.0.${CIRCLE_BUILD_NUM}
+docker tag ${FLAKE_DOCKER_IMAGE_NAME}:latest ${AWS_ECR_ACCOUNT_URL}/${FLAKE_DOCKER_IMAGE_NAME}:0.0.${CIRCLE_SHA1:0:7}
+docker push ${AWS_ECR_ACCOUNT_URL}/${FLAKE_DOCKER_IMAGE_NAME}:0.0.${CIRCLE_SHA1:0:7}
