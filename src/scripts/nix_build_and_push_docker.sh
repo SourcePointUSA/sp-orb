8872429dace0592204177906a23d19ac096ccc8b
drcarney-sp <98330282+drcarney-sp@users.noreply.github.com>
2022-11-10 14:46:39 +0100

    fix nix build script (#3)
    * fix nix build script
    
    * Update nix_build_and_push_docker.sh

--- src/scripts/nix_build_and_push_docker.sh
+++ src/scripts/nix_build_and_push_docker.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 FLAKE_DOCKER_IMAGE_NAME=$(eval echo "$PARAM_FLAKE_DOCKER_IMAGE_NAME")
 
-nix build
-set -e
-sudo chmod -R 777 /nix/store/$(ls /nix/store/ | grep crates-io | grep -v '.drv$')
-nix build
 nix build .#${FLAKE_DOCKER_IMAGE_NAME}
+EXIT_CODE=$?
+
+set -e
+
+# TODO figure out if we still need this crap
+if [ $EXIT_CODE -ne 0 ]
+then
+  echo "double build!"
+  sudo chmod -R 777 /nix/store/$(ls /nix/store/ | grep crates-io | grep -v '.drv$')
+  nix build .#${FLAKE_DOCKER_IMAGE_NAME}
+fi
+
 docker load < result 
 docker tag ${FLAKE_DOCKER_IMAGE_NAME}:latest ${AWS_ECR_ACCOUNT_URL}/${FLAKE_DOCKER_IMAGE_NAME}:latest
 docker push ${AWS_ECR_ACCOUNT_URL}/${FLAKE_DOCKER_IMAGE_NAME}:latest
